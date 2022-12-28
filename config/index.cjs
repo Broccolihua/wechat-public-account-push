dedd4c3998e86b31fbf590e93a69502b00829112
Broccolihua <86149609+Broccolihua@users.noreply.github.com>
2022-12-28 10:11:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '华宜赛', year: '1998', date: '09-02',
+          type: '生日', name: '华宜赛', year: '1998', date: '08-27',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '10-08',
