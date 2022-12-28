116360530f95d865bb78859e19f11771d4e20f9b
Broccolihua <86149609+Broccolihua@users.noreply.github.com>
2022-12-28 10:04:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oYgpw6anzlV3OgDKMoohdPqSiUyc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3fVTGz2uxgA5dH9cqQCuOpSytXMcWgtG47N9vWhJO2k',
+      useTemplateId: 'UwptSimcwSLJMl_f8NEavbIXmd-gqE0KHL_mpLr6eZk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '1-11',
       festivals: [
